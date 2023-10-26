f3d5078ebafed0b9429c065eb792b2f914b911c4
Florent <duparquet.florent@orange.fr>
2023-10-26 14:36:59 +0200

    test 2

--- meteo.js
+++ meteo.js
@@ -15,11 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 fetch(apiUrl)
                     .then(response => response.json())
                     .then(data => {
-                        const weatherInfo = document.getElementById("info-meteo");
-                        weatherInfo.innerHTML = `Conditions météorologiques à ${ville}: ${data.weather[0].description}`;
+                        const infoMeteo = document.getElementById("info-meteo");
+                        infoMeteo.innerHTML = `${data.weather[0].description} `;
+                        const icone = document.getElementById("icone");
+                        icone.innerHTML = `<img src=https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png></img>`;
                         const temperature = document.getElementById("temperature");
-                        temperature.innerHTML = `la température à ${ville} sera de : ${data.main.temp} degrés`;
-                        
+                        temperature.innerHTML = ` ${data.main.temp} ° C`;
+                        const localisation = document.getElementById("loc");
+                        localisation.innerHTML = ` ${ville}`;
                     })
                     .catch(error => {
                         console.error("Erreur lors de la récupération des données météo : " + error);
@@ -29,8 +32,8 @@ document.addEventListener("DOMContentLoaded", function () {
             // Appel initial pour afficher les données météo
             fetchWeatherData();
 
-            // Mise à jour des données météo toutes les heures
-            setInterval(fetchWeatherData, 3600000); // 3600000 ms = 1 heure
+            // Mise à jour des données météo toutes les heures, 3600000 ms = 1 heure
+            setInterval(fetchWeatherData, 3600000); 
         })
         .catch(error => {
             console.error("Erreur lors de la récupération de la clé API : " + error);
